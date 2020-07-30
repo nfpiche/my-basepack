9ca9317010da1c6e967127c4b187e256510b7bbd
Nate Piche <nate.f.piche@gmail.com>
2020-07-30 10:57:18 -0400

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const dotenv = require('dotenv');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const path = require("path");
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const dotenv = require("dotenv");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const InlineManifestWebpackPlugin = require("@insanecoding/inline-manifest-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-const parts = require('./webpack.parts');
+const parts = require("./webpack.parts");
 
 const PATHS = {
-  app: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'dist'),
+  app: path.join(__dirname, "src"),
+  build: path.join(__dirname, "dist")
 };
 
 const envPath = path.join(__dirname, `.env.${process.env.NODE_ENV}`);
@@ -19,17 +19,17 @@ const envVars = dotenv.config({ path: envPath }).parsed;
 const production = merge([
   {
     entry: [
-      require.resolve('core-js/stable'),
-      require.resolve('regenerator-runtime/runtime'),
-      PATHS.app,
-    ],
+      require.resolve("core-js/stable"),
+      require.resolve("regenerator-runtime/runtime"),
+      PATHS.app
+    ]
   },
   {
     output: {
-      chunkFilename: '[name].[chunkhash:8].js',
-      filename: '[name].[chunkhash:8].js',
-      publicPath: process.env.PUBLIC_PATH,
-    },
+      chunkFilename: "[name].[chunkhash:8].js",
+      filename: "[name].[chunkhash:8].js",
+      publicPath: process.env.PUBLIC_PATH
+    }
   },
   parts.extractCSS(),
   parts.clean(PATHS.build),
@@ -37,34 +37,37 @@ const production = merge([
   parts.minifyCSS({
     options: {
       discardComments: {
-        removeAll: false,
+        removeAll: false
       },
-      safe: true,
-    },
+      safe: true
+    }
   }),
   {
     optimization: {
       splitChunks: {
-        chunks: 'initial',
+        chunks: "initial"
       },
       runtimeChunk: {
-        name: 'manifest',
-      },
-    },
+        name: "manifest"
+      }
+    }
   },
+  {
+    plugins: [new InlineManifestWebpackPlugin("manifest")]
+  }
 ]);
 
 const development = merge([
   parts.devServer,
   parts.loadCSS(),
   {
-    plugins: [new webpack.HotModuleReplacementPlugin()],
-  },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+  }
 ]);
 
 const envs = {
   production,
-  development,
+  development
 };
 
 module.exports = (mode, opts = {}) => {
@@ -72,16 +75,15 @@ module.exports = (mode, opts = {}) => {
     {
       plugins: [
         new HtmlWebpackPlugin({
-          title: 'BASEPACK',
-          template: path.join(__dirname, 'src/index.html'),
-          ...envVars,
-        }),
-        new InlineManifestWebpackPlugin('manifest'),
-      ],
+          title: "BASEPACK",
+          template: path.join(__dirname, "src/index.html"),
+          ...envVars
+        })
+      ]
     },
     parts.loadJS({ include: PATHS.app }),
     parts.setEnvironmentVariables(envVars),
-    opts.shouldAnalyze ? { plugins: [new BundleAnalyzerPlugin()] } : {},
+    opts.shouldAnalyze ? { plugins: [new BundleAnalyzerPlugin()] } : {}
   ]);
 
   return merge(common, envs[mode], { mode });
